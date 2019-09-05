2b13967efd7220e53cec079f551d52959f0951af
Sergey Kolchenko <kolchenkosergey@gmail.com>
2019-09-04 23:13:13 -0400

    added files for proper training and cv

--- train.py
+++ train.py
@@ -1,305 +1,114 @@
-import torch
+import sys
+import torch 
 import os
+import random
 import torch.nn as nn
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import cohen_kappa_score, f1_score, roc_auc_score
+import pandas as pd 
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torch.optim import Adam, SGD, RMSprop
+import time
+from torch.autograd import Variable
+import torch.functional as F
+from tqdm import tqdm
+from sklearn import metrics
+import urllib
+from sklearn.metrics import cohen_kappa_score, mean_squared_error, mean_absolute_error
+import pickle
 import cv2
+import torch.nn.functional as F
 from torchvision import models
-
+import seaborn as sns
+import random
+import sys
 import collections
-from torch.utils.data import Dataset, DataLoader
-from albumentations import (Normalize, Resize, Compose)
-from catalyst.dl import MetricCallback
-from catalyst.contrib.schedulers import ReduceLROnPlateau
+import fire
+from torch.utils.data import Dataset, WeightedRandomSampler, SubsetRandomSampler, DataLoader
+from albumentations import (
+    HorizontalFlip, VerticalFlip, CenterCrop, RandomRotate90, RandomCrop, 
+    PadIfNeeded, Normalize, Flip, OneOf, Compose, Resize, Transpose, 
+    IAAAdditiveGaussianNoise, GaussNoise, CLAHE, RandomBrightnessContrast, HueSaturationValue,
+    HorizontalFlip, IAAPerspective, ShiftScaleRotate, CLAHE, RandomRotate90,
+    Transpose, ShiftScaleRotate, Blur, OpticalDistortion, GridDistortion, HueSaturationValue,
+    IAAAdditiveGaussianNoise, GaussNoise, MotionBlur, MedianBlur, RandomBrightnessContrast, IAAPiecewiseAffine,
+    IAASharpen, IAAEmboss, Flip, OneOf, Compose
+)
+from catalyst.contrib.schedulers import OneCycleLR, ReduceLROnPlateau, StepLR, MultiStepLR
+from catalyst.dl.experiment import SupervisedExperiment
 from catalyst.dl.runner import SupervisedRunner
-from catalyst.dl.callbacks import EarlyStoppingCallback
-
-
-def crop_image_from_gray(img, tol=7):
-    if img.ndim == 2:
-        mask = img > tol
-        return img[np.ix_(mask.any(1), mask.any(0))]
-    elif img.ndim == 3:
-        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        mask = gray_img > tol
-
-        check_shape = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))].shape[0]
-        if check_shape == 0:  # image is too dark so that we crop out everything,
-            return img  # return original image
-        else:
-            img1 = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))]
-            img2 = img[:, :, 1][np.ix_(mask.any(1), mask.any(0))]
-            img3 = img[:, :, 2][np.ix_(mask.any(1), mask.any(0))]
-            img = np.stack([img1, img2, img3], axis=-1)
-        return img
-
-
-def weighted_kappa(
-        outputs: torch.Tensor,
-        targets: torch.Tensor,
-        threshold: float = None,
-        activation: str = None
-):
-    """
-    Args:
-        outputs (torch.Tensor): A list of predicted elements
-        targets (torch.Tensor):  A list of elements that are to be predicted
-        activation (str): An torch.nn activation applied to the outputs.
-            Must be one of ["none", "Sigmoid", "Softmax2d"]
-    Returns:
-        float: quadratic kappa score
-    """
-    outputs = outputs.detach().cpu().numpy()
-    targets = targets.detach().cpu().numpy()
-    outputs = outputs.flatten()
-    targets = targets.flatten()
-    outputs_clipped = outputs
-    outputs_clipped[outputs_clipped < 0.5] = 0
-    outputs_clipped[outputs_clipped >= 0.5] = 1
-    score = cohen_kappa_score(outputs_clipped, targets)
-    return score
-
-
-class KappScoreMetricCallback(MetricCallback):
-    """
-    F1 score metric callback.
-    """
-
-    def __init__(
-            self,
-            input_key: str = "targets",
-            output_key: str = "logits",
-            prefix: str = "qkappa_score",
-            activation: str = None
-    ):
-        """
-        Args:
-            input_key (str): input key to use for iou calculation
-                specifies our ``y_true``.
-            output_key (str): output key to use for iou calculation;
-                specifies our ``y_pred``
-            activation (str): An torch.nn activation applied to the outputs.
-                Must be one of ['none', 'Sigmoid', 'Softmax2d']
-        """
-
-        super().__init__(
-            prefix=prefix,
-            metric_fn=weighted_kappa,
-            input_key=input_key,
-            output_key=output_key,
-            activation=activation
-        )
-
-
-def rocauc(
-        outputs: torch.Tensor,
-        targets: torch.Tensor,
-        threshold: float = None,
-        activation: str = None
-):
-    """
-    Args:
-        outputs (torch.Tensor): A list of predicted elements
-        targets (torch.Tensor):  A list of elements that are to be predicted
-        activation (str): An torch.nn activation applied to the outputs.
-            Must be one of ["none", "Sigmoid", "Softmax2d"]
-    Returns:
-        float: quadratic kappa score
-    """
-    outputs = outputs.detach().cpu().numpy()
-    targets = targets.detach().cpu().numpy()
-    outputs = outputs.flatten()
-    targets = targets.flatten()
-    outputs_clipped = outputs
-    outputs_clipped[outputs_clipped < 0.5] = 0
-    outputs_clipped[outputs_clipped >= 0.5] = 1
-    score = roc_auc_score(outputs, targets)
-    return score
-
-
-class RocAucCallback(MetricCallback):
-    """
-    F1 score metric callback.
-    """
-
-    def __init__(
-            self,
-            input_key: str = "targets",
-            output_key: str = "logits",
-            prefix: str = "rocauc_score",
-            activation: str = None
-    ):
-        """
-        Args:
-            input_key (str): input key to use for iou calculation
-                specifies our ``y_true``.
-            output_key (str): output key to use for iou calculation;
-                specifies our ``y_pred``
-            activation (str): An torch.nn activation applied to the outputs.
-                Must be one of ['none', 'Sigmoid', 'Softmax2d']
-        """
-
-        super().__init__(
-            prefix=prefix,
-            metric_fn=rocauc,
-            input_key=input_key,
-            output_key=output_key,
-            activation=activation
-        )
-
-
-def f1score(
-        outputs: torch.Tensor,
-        targets: torch.Tensor,
-        threshold: float = None,
-        activation: str = None
-):
-    """
-    Args:
-        outputs (torch.Tensor): A list of predicted elements
-        targets (torch.Tensor):  A list of elements that are to be predicted
-        activation (str): An torch.nn activation applied to the outputs.
-            Must be one of ["none", "Sigmoid", "Softmax2d"]
-    Returns:
-        float: quadratic kappa score
-    """
-    outputs = outputs.detach().cpu().numpy()
-    targets = targets.detach().cpu().numpy()
-    outputs = outputs.flatten()
-    targets = targets.flatten()
-    outputs_clipped = outputs
-    outputs_clipped[outputs_clipped < 0.5] = 0
-    outputs_clipped[outputs_clipped >= 0.5] = 1
-    score = f1_score(outputs_clipped, targets, average='micro')
-    return score
-
-
-class F1Callback(MetricCallback):
-    """
-    F1 score metric callback.
-    """
-
-    def __init__(
-            self,
-            input_key: str = "targets",
-            output_key: str = "logits",
-            prefix: str = "f1_score",
-            activation: str = None
-    ):
-        """
-        Args:
-            input_key (str): input key to use for iou calculation
-                specifies our ``y_true``.
-            output_key (str): output key to use for iou calculation;
-                specifies our ``y_pred``
-            activation (str): An torch.nn activation applied to the outputs.
-                Must be one of ['none', 'Sigmoid', 'Softmax2d']
-        """
-
-        super().__init__(
-            prefix=prefix,
-            metric_fn=f1score,
-            input_key=input_key,
-            output_key=output_key,
-            activation=activation
-        )
-
-
-class EyesDataset(Dataset):
-    def __init__(self, dataset_path, names, labels, augmentations):
-        self.labels = labels
-        self.names = names
-        self.dataset_path = dataset_path
-        self.augmentations = augmentations
-
-    def __len__(self):
-        return len(self.names)
-
-    def __getitem__(self, index):
-        image = cv2.imread(os.path.join(self.dataset_path, self.names[index]))[..., :: -1]
-        image = crop_image_from_gray(image)
-        if self.augmentations:
-            augmented = self.augmentations(image=image)
-            image = augmented['image']
-        target = self.labels[index]
-        return torch.from_numpy(image.transpose((2, 0, 1))).float(), torch.tensor(target).float()
-
-
-def aug_train(resolution, p=1):
-    return Compose([Resize(resolution, resolution),
-                    Normalize()], p=p)
-
-
-def aug_val(resolution, p=1):
-    return Compose([Resize(resolution, resolution), Normalize()], p=p)
-
-
+from catalyst.dl.callbacks import EarlyStoppingCallback, AccuracyCallback, F1ScoreCallback, ConfusionMatrixCallback, MixupCallback
+from catalyst.dl.core.state import RunnerState
+from catalyst.dl.core import MetricCallback
+from catalyst.dl.callbacks import CriterionCallback
+from efficientnet_pytorch import EfficientNet
+from utils import *
 if __name__ == '__main__':
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    num_classes = 8
-    lr = 3e-5
-    IMG_SIZE = 256
-
-    train = pd.read_csv('data/splited_train.csv')
-    X = train['id'].values
-    train = train.drop(columns='id')
-    X_train, X_val, y_train, y_val = train_test_split(X, train.values.tolist(), test_size=0.2, random_state=42)
-
-    model = models.resnet50(pretrained=True)
-    model.fc = nn.Linear(2048, num_classes)
-    model.cuda()
-
-    train_dataset = EyesDataset(dataset_path='data/ODIR-5K_Training_Images',
-                                labels=y_train,
-                                names=X_train,
-                                augmentations=aug_train(IMG_SIZE))
-
-    val_dataset = EyesDataset(dataset_path='data/ODIR-5K_Training_Images',
-                              labels=y_val,
-                              names=X_val,
-                              augmentations=aug_val(IMG_SIZE))
-
-    train_loader = DataLoader(train_dataset,
-                              num_workers=16,
-                              pin_memory=False,
-                              batch_size=5,
-                              shuffle=True)
-    val_loader = DataLoader(val_dataset,
-                            num_workers=16,
-                            pin_memory=False,
-                            batch_size=5)
-
-    loaders = collections.OrderedDict()
-    loaders["train"] = train_loader
-    loaders["valid"] = val_loader
-    runner = SupervisedRunner()
-    logdir = f"logs/baseline_crop_from_gray"
-
-    for p in model.parameters():
-        p.requires_grad = True
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    num_epochs = 20
-    criterion = nn.BCEWithLogitsLoss()
-    scheduler = ReduceLROnPlateau(optimizer=optimizer, factor=0.75, patience=5)
-    runner.train(
-        model=model,
-        criterion=criterion,
-        optimizer=optimizer,
-        loaders=loaders,
-        logdir=logdir,
-        scheduler=scheduler,
-        callbacks=[
-            KappScoreMetricCallback(),
-            F1Callback(),
-            # RocAucCallback(),
-            EarlyStoppingCallback(patience=25, metric='loss')
-        ],
-        num_epochs=num_epochs,
-        verbose=True
-    )
-
-
-
-
+	splits = pickle.load(open('cv_split.pickle', 'rb'))
+	data = pd.read_csv('./data/splited_train.csv')
+	labels  = ['N','D','G','C','A','H','M','O']
+	n_classes = len(labels)
+	fold_idx, batch_size, model_name, image_size, head_n_epochs, head_lr, full_n_epochs, full_lr, exp_name = fire.Fire(arguments)
+	os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+	num_classes = len(labels)
+	seed_everything(1234)
+	runner = SupervisedRunner()
+	model = prepare_model(model_name, n_classes)	
+	train_path = './ODIR-5K_Training_Dataset/'
+	valid_path = './ODIR-5K_Training_Dataset/'
+	train_dataset = EyeDataset(dataset_path = train_path, 
+					labels=data.loc[splits['train_idx'][fold_idx],labels].values, 
+					ids=data.loc[splits['train_idx'][fold_idx],'id'].values, 
+					albumentations_tr=aug_train_heavy(image_size)) 
+	val_dataset = EyeDataset(dataset_path=valid_path, 
+					labels=data.loc[splits['train_idx'][fold_idx],labels].values, 
+					ids=data.loc[splits['train_idx'][fold_idx],'id'].values, 
+					albumentations_tr=aug_val(image_size))
+	train_loader =  DataLoader(train_dataset,
+					num_workers=4,
+					pin_memory=False,
+					batch_size=batch_size,
+					shuffle=True)
+	val_loader =  DataLoader(val_dataset,
+					num_workers=4,
+					pin_memory=False,
+					batch_size=batch_size,
+					shuffle=True)	
+	loaders = collections.OrderedDict()
+	loaders["train"] = train_loader
+	loaders["valid"] = val_loader
+	logdir = 'logs/{}_fold{}/'.format(exp_name, fold_idx)
+	print('Training only head for {} epochs with inital lr {}'.format(head_n_epochs, head_lr))
+	for p in model.parameters():
+		p.requires_grad = False
+	for p in model._fc.parameters():
+		p.requires_grad = True
+	optimizer = torch.optim.Adam(model.parameters(), lr=head_lr)
+	criterion = nn.CrossEntropyLoss()
+	scheduler = ReduceLROnPlateau(optimizer=optimizer, factor=0.75, patience=2)
+	runner.train(model=model,
+			criterion=criterion,
+			optimizer=optimizer,
+			loaders=loaders,
+			logdir=logdir,
+			scheduler=scheduler,
+			callbacks=[
+				],
+			num_epochs=head_n_epochs,
+			verbose=True)      
+	print('Train whole net for {} epochs with initial lr {}'.format(full_n_epochs, full_lr))
+	for p in model.parameters():
+		p.requires_grad = True
+	optimizer = torch.optim.Adam(model.parameters(), lr=full_lr)
+	criterion = nn.CrossEntropyLoss()
+	scheduler = ReduceLROnPlateau(optimizer=optimizer, factor=0.75, patience=2)
+	runner.train(model=model,
+			criterion=criterion,
+			optimizer=optimizer,
+			loaders=loaders,
+			logdir=logdir,
+			scheduler=scheduler,
+			callbacks=[
+				],
+			num_epochs=full_n_epochs,
+			verbose=True)    	
---
+++ utils.py
@@ -0,0 +1,117 @@
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import random
+import os
+from albumentations import (
+    HorizontalFlip, VerticalFlip, CenterCrop, RandomRotate90, RandomCrop, 
+    PadIfNeeded, Normalize, Flip, OneOf, Compose, Resize, Transpose, 
+    IAAAdditiveGaussianNoise, GaussNoise, CLAHE, RandomBrightnessContrast, HueSaturationValue,
+    HorizontalFlip, IAAPerspective, ShiftScaleRotate, CLAHE, RandomRotate90,
+    Transpose, ShiftScaleRotate, Blur, OpticalDistortion, GridDistortion, HueSaturationValue,
+    IAAAdditiveGaussianNoise, GaussNoise, MotionBlur, MedianBlur, RandomBrightnessContrast, IAAPiecewiseAffine,
+    IAASharpen, IAAEmboss, Flip, OneOf, Compose
+)
+from torch.utils.data import Dataset
+from efficientnet_pytorch import EfficientNet
+def crop_image_from_gray(img, tol=7):
+    if img.ndim == 2:
+        mask = img > tol
+        return img[np.ix_(mask.any(1), mask.any(0))]
+    elif img.ndim == 3:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        mask = gray_img > tol
+
+        check_shape = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))].shape[0]
+        if (check_shape == 0):  # image is too dark so that we crop out everything,
+            return img  # return original image
+        else:
+            img1 = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))]
+            img2 = img[:, :, 1][np.ix_(mask.any(1), mask.any(0))]
+            img3 = img[:, :, 2][np.ix_(mask.any(1), mask.any(0))]
+            img = np.stack([img1, img2, img3], axis=-1)
+        return img
+
+def arguments(fold_idx, batch_size, model_name, image_size, head_n_epochs, head_lr, full_n_epochs, full_lr, exp_name):
+	print('Will run with the following arguments: \n fold_idx : {}, model_name : {}, batch_size : {} \n image_size : {}, head_n_epochs : {}, head_lr : {}, full_n_epochs : {}, full_lr : {}, exp_name : {}'.format(fold_idx, batch_size, model_name, image_size, head_n_epochs, head_lr, full_n_epochs, full_lr, exp_name))
+	return(fold_idx, batch_size, model_name, image_size, head_n_epochs, head_lr, full_n_epochs, full_lr, exp_name)
+
+def prepare_model(model_name, n_classes):
+	print('Will load {}'.format(model_name))
+	model = EfficientNet.from_pretrained(model_name)
+	in_features = model._fc.in_features
+	model._fc = nn.Linear(in_features, n_classes)
+	model.cuda()
+	return(model)		
+
+def seed_everything(seed):
+	random.seed(seed)
+	os.environ['PYTHONHASHSEED'] = str(seed)
+	np.random.seed(seed)
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed(seed)
+	torch.backends.cudnn.deterministic = True
+
+class EyeDataset(Dataset):
+    def __init__(self, dataset_path, labels, ids, albumentations_tr=False, shuffle=True):
+        self.labels = labels
+        self.ids = ids
+        self.shuffle = shuffle
+        self.dataset_path = dataset_path
+        self.albumentations_tr = albumentations_tr
+            
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, index):
+        #imid = self.ids[index]
+        image = cv2.imread(os.path.join(self.dataset_path, self.ids[index]))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = crop_image_from_gray(image)
+        if self.albumentations_tr:
+            augmented = self.albumentations_tr(image=image)
+            image = augmented['image']
+        target = np.argmax(self.labels[index])
+        return torch.from_numpy(image.transpose((2, 0, 1))).float(), torch.tensor(np.expand_dims(target,0)).long()
+
+def aug_train(resolution,p=1): 
+    return Compose([Resize(resolution,resolution),
+                    OneOf([
+                        HorizontalFlip(), 
+                        VerticalFlip(), 
+                        RandomRotate90(), 
+                        Transpose()],p=0.5),
+                    Normalize()
+                    ], p=p)
+def aug_train_heavy(resolution, p=1.0):
+    return Compose([
+        Resize(resolution,resolution),
+        OneOf([RandomRotate90(),Flip(),Transpose(),HorizontalFlip(),VerticalFlip()],p=1.0),
+        OneOf([
+            IAAAdditiveGaussianNoise(),
+            GaussNoise(),
+        ], p=0.5),
+        OneOf([
+            MotionBlur(p=.2),
+            MedianBlur(blur_limit=3, p=0.1),
+            Blur(blur_limit=3, p=0.1),
+        ], p=0.2),
+        ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
+        OneOf([
+            OpticalDistortion(p=0.3),
+            GridDistortion(p=.1),
+            IAAPiecewiseAffine(p=0.3),
+        ], p=0.1),
+        OneOf([
+            CLAHE(clip_limit=2),
+            IAASharpen(),
+            IAAEmboss(),
+            RandomBrightnessContrast(),            
+        ], p=0.3),
+        HueSaturationValue(p=0.3),
+        Normalize()
+    ], p=p)
+
+def aug_val(resolution,p=1): 
+    return Compose([Resize(resolution,resolution),Normalize()], p=p)
