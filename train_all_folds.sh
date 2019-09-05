ef03b89df0288d4b59dea968a443e2f3413c7034
azkalot1 <kolchenkosergey@gmail.com>
2019-09-05 19:35:17 +0200

    added file for whole process

--- train_all_folds.sh
+++ train_all_folds.sh
@@ -4,3 +4,5 @@ python3 train.py 1 32 efficientnet-b3 240 5 1e-5 25 3e-4 initial_test_b3
 python3 train.py 2 32 efficientnet-b3 240 5 1e-5 25 3e-4 initial_test_b3
 python3 train.py 3 32 efficientnet-b3 240 5 1e-5 25 3e-4 initial_test_b3
 python3 train.py 4 32 efficientnet-b3 240 5 1e-5 25 3e-4 initial_test_b3
+python3 CrossValidationScore.py 0 32 efficientnet-b3 240 5 1e-5 25 3e-4 initial_test_b3
+python3 Predictions.py 0 32 efficientnet-b3 240 5 1e-5 25 3e-4 initial_test_b3
